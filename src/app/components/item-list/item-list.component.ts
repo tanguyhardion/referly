d06a1642bce418014b7ead9e46fe18780068e635
tanguyhardion <45904983+tanguyhardion@users.noreply.github.com>
2023-08-29 21:44:16 +0200

    item list component

---
+++ src/app/components/item-list/item-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-item-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './item-list.component.html',
+  styleUrls: ['./item-list.component.css']
+})
+export class ItemListComponent {
+
+}
